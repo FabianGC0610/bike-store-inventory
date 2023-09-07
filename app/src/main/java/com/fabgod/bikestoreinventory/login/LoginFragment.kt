42ccd4195b6e020c5145a087f54aabeb53e49143
Fabian G <fabian.god06@gmail.com>
2023-09-06 19:17:07 -0600

    feat: welcome screen design is added

--- app/src/main/java/com/fabgod/bikestoreinventory/login/LoginFragment.kt
+++ app/src/main/java/com/fabgod/bikestoreinventory/login/LoginFragment.kt
@@ -4,20 +4,37 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.fabgod.bikestoreinventory.R
+import com.fabgod.bikestoreinventory.databinding.LoginFragmentBinding
 
 /**
  * Fragment where the user can set his credentials (log in or create account)
  */
 class LoginFragment : Fragment() {
 
+    private lateinit var binding: LoginFragmentBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.login_fragment, container, false)
+    ): View {
+        // Inflate view and obtain an instance of the binding class
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.login_fragment,
+            container,
+            false,
+        )
+
+        binding.button.setOnClickListener {
+            val action = LoginFragmentDirections.actionLoginFragmentToWelcomeFragment()
+            findNavController().navigate(action)
+        }
+
+        return binding.root
     }
 }
