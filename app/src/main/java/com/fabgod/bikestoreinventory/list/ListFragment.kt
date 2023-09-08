d7da7143c2f8c1e8d4f1ed7e1cd08263caa55bc5
Fabian G <fabian.god06@gmail.com>
2023-09-07 19:20:59 -0600

    feat: add bike section design is added

--- app/src/main/java/com/fabgod/bikestoreinventory/list/ListFragment.kt
+++ app/src/main/java/com/fabgod/bikestoreinventory/list/ListFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.fabgod.bikestoreinventory.R
+import com.fabgod.bikestoreinventory.databinding.ListFragmentBinding
 
 /**
  * Fragment where the list of items (bikes) is shown and
@@ -13,12 +16,30 @@ import com.fabgod.bikestoreinventory.R
  */
 class ListFragment : Fragment() {
 
+    private lateinit var binding: ListFragmentBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.list_fragment, container, false)
+    ): View {
+        // Inflate view and obtain an instance of the binding class
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.list_fragment,
+            container,
+            false,
+        )
+
+        binding.addButton.setOnClickListener {
+            val action = ListFragmentDirections.actionListFragmentToDetailsFragment()
+            findNavController().navigate(action)
+        }
+
+        binding.menuBar.backArrow.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
+        return binding.root
     }
 }
